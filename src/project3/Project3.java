4dc1b8b50f1ce45f482b69365942810a28c55007
jamesosborne <james.osborne112@gmail.com>
2016-09-26 18:38:35 -0400

    Created main class

---
+++ src/project3/Project3.java
@@ -0,0 +1,18 @@
+package project3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Owner
+ */
+public class Project3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner();
+    }
+    
+}
